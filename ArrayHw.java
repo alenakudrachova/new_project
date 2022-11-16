d9d390b990ce1f63619b829b65ab529cc47ea7d4
alenakudrachova <keeed777@mail.ru>
2022-11-16 23:08:34 +0300

    arrays homework

---
+++ ArrayHw.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayHw {
+    public static void array_avr() {
+        int[] arrays = new int[55];
+        int m = 0;
+        int s = 0;
+        Random random = new Random();
+
+        for (int n = 0; n < arrays.length; n++) {
+            arrays[n] = random.nextInt();
+            m = m + arrays[n];
+        }
+        s = m / 55;
+        System.out.println(s);
+    }
+    public static void max_num() {
+        int[] arrays2 = new int[5];
+        Random random = new Random();
+
+        for (int m = 0; m < arrays2.length; m++) {
+            arrays2[m] = random.nextInt();
+        }
+            int maxNum = arrays2[0];
+            for (int a : arrays2) {
+            if (a > maxNum)
+                maxNum = a;
+        }
+        System.out.println(maxNum);
+    }
+
+        public static void main (String[]args) {
+            array_avr();
+            max_num();
+        }
+}
