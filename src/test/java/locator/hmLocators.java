180cbbf9b7f7f3484fea088bb76e6ac77a575b4c
alenakudrachova <keeed777@mail.ru>
2023-02-01 21:12:26 +0300

    fixes

--- src/test/java/locator/hmLocators.java
+++ src/test/java/locator/hmLocators.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.By;
 public class hmLocators {
     By linkTwitter = By.linkText("Twitter");
     By partLinkTextFacebook = By.partialLinkText("Face");
-    By divTag = By.tagName("div");
+    By selectTag = By.tagName("select");
     By elementProducts = By.className("title");
     By nameButtonCheckout = By.name("checkout");
-    By findId = By.id("[id^=add-to-cart]");
+    By findId = By.id("add-to-cart-sauce-labs-bolt-t-shirt");
     By allElements = By.cssSelector("*");
     By allButtons = By.cssSelector("[id^=add-to-cart]");
-    By ElementsById = By.cssSelector("#header_container *");
-    By ElementByClass = By.cssSelector("[class='inventory_item_name']");
+    By elementsButtons = By.cssSelector("#header_container button");
+    By elementByClass = By.cssSelector("[class='inventory_item_name']");
     By dataTestAddToCardButton = By.cssSelector("[data-test^='add-to-cart']");
 }
